414bc726d006523129d4836386da9a805c32c0d8
blackymax <maxmin3007@gmail.com>
2022-10-03 10:27:01 +0300

    feat: added sns email sending

--- product-service/src/handlers/catalogBatchProcess.ts
+++ product-service/src/handlers/catalogBatchProcess.ts
@@ -12,8 +12,10 @@ const catalogBatchProcess = (event: any, context: any, cb: any) => {
             const product = isJson(data.messageAttributes)
                 ? JSON.parse(data.messageAttributes)
                 : data.messageAttributes;
+            
             console.log('Data: ' + JSON.stringify(data));
-            console.log('Product: ' + data.body);
+            console.log('Product: ' + JSON.stringify(data.body));
+
             const productNew = {
                 title: product.title.stringValue,
                 description: product.description.stringValue,
@@ -21,28 +23,33 @@ const catalogBatchProcess = (event: any, context: any, cb: any) => {
                 count: product.count.stringValue
             };
             const { title, description, price, count } = productNew;
+
             console.log(title, description, price, count);
+
             productProviderDynamoDB.createProduct(
                 title,
                 description,
                 price,
                 count
             );
+
+            const publishParams: PublishInput = {
+                Subject: 'Products has been created',
+                Message: "Success",
+                MessageAttributes: data.messageAttributes,
+                TopicArn: 'arn:aws:sns:eu-west-1:673313573473:createProductTopic',
+                
+            };
+
+            sns.publish(publishParams, (err, data) => {
+                if (err) {
+                    console.log('SNS:ERROR: ' + JSON.stringify(err));
+                } else {
+                    console.log('SNS:DATA: ' + JSON.stringify(data));
+                }
+            })
+            cb(null, HttpResponse.success({}));
         });
-        const publishParams: PublishInput = {
-            Subject: 'Products has been created',
-            Message: "Success",
-            // MessageAttributes: products[0],
-            TopicArn: 'arn:aws:sns:eu-west-1:673313573473:createProductTopic'
-        };
-        sns.publish(publishParams, (err, data) => {
-            if (err) {
-                console.log('SNS:ERROR: ' + JSON.stringify(err));
-            } else {
-                console.log('SNS:DATA: ' + JSON.stringify(data));
-            }
-        })
-        cb(null, HttpResponse.success({}));
     } catch (err) {
         cb(null, HttpResponse.serverError(err));
     }
